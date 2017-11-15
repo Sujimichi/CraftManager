83968943a25e7a25b3a1ef49f27247ae76acaf63
Sujimichi <sujimichi@gmail.com>
2017-11-15 19:34:28 +0000

    initial commit

---
+++ CraftManager.cs
@@ -0,0 +1,23 @@
+﻿
+
+//Built Against KSP 1.3.1
+//build id = 01891
+//2017-10-05_22-01-21
+//Branch: master
+
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using KSP.UI.Screens;
+
+namespace CraftManager
+{
+
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class CraftManager : MonoBehaviour
+    {
+        
+    }
+}
+
---
+++ scripts/collie.sh
@@ -0,0 +1,25 @@
+#Collie - rounds all the files up into release
+
+rm -rf bin/Release/CraftManager
+rm bin/Release/CraftManager.zip
+
+mkdir bin/Release/CraftManager -p
+mkdir bin/Release/CraftManager/Plugins -p
+
+cp bin/Release/CraftManager.dll bin/Release/CraftManager/Plugins/CraftManager.dll
+
+cp -a Assets/*.* bin/Release/CraftManager/
+cp LICENCE.txt bin/Release/CraftManager/LICENCE.txt
+
+#ruby -e "i=%x(cat Source/KerbalX.cs | grep version); i=i.split('=')[1].sub(';','').gsub('\"','').strip; s=\"echo 'version: #{i}' > bin/Release/KerbalX/version\"; system(s)"
+
+
+rm bin/Release/CraftManager.dll
+rm bin/Release/CraftManager.dll.mdb
+
+cd bin/Release
+zip -r CraftManager.zip CraftManager/
+
+
+rm -rf /home/sujimichi/KSP/dev_KSP-1.3.1/GameData/CraftManager/
+cp -R CraftManager/ /home/sujimichi/KSP/dev_KSP-1.3.1/GameData/CraftManager/
